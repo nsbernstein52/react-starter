0952e75bae97933b68c1cd6db5be97f25ed736d9
nsbernstein52 <57200889+nsbernstein52@users.noreply.github.com>
2020-02-20 10:34:01 -0700

    Code level 1

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,42 @@ class App extends React.Component {
   render(){
     return(
     <div>
-      Starter App component - hello there
+      <table 
+        id="movieList"
+      >
+        <h3>
+        Movie List
+        </h3>
+      </table>
+      <table id="ovies" >
+        <tr key="0" className="singleMovieEntry">
+          {movies[0].title}
+        </tr>
+        <tr key="1" className="singleMovieEntry">
+          {movies[1].title}
+        </tr>
+        <tr key="2" className="singleMovieEntry">
+          {movies[2].title}
+        </tr>
+        <tr key="3" className="singleMovieEntry">
+          {movies[3].title}
+        </tr>
+        <tr key="4" className="singleMovieEntry">
+          {movies[4].title}
+        </tr>
+      </table>
     </div>
   )}
 }
 
+var movies = [
+  {title: 'Mean Girls'},
+  {title: 'Hackers'},
+  {title: 'The Grey'},
+  {title: 'Sunshine'},
+  {title: 'Ex Machina'},
+];
+
 export default App;
+// stuff
+// Starter App component - hello there
