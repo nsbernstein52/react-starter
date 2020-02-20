43b59d16e6930d6904352e23f5a2d657ce256192
nsbernstein52 <57200889+nsbernstein52@users.noreply.github.com>
2020-02-20 15:26:12 -0700

    Add user-add movies

--- src/components/App.js
+++ src/components/App.js
@@ -9,12 +9,18 @@ class App extends React.Component {
       movieList: [],
       moviesListStr: '',
       currMovie: null,
-      text: ''
+      text: '',
+      userMoviesArr: [],
+      userMoviesListStr: '',
+      currUserMovie: null,
+      userText: '',
     };
 
     this.changeCurrMovie = this.changeCurrMovie.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.handleUserChange = this.handleUserChange.bind(this);
+    this.handleUserClick = this.handleUserClick.bind(this);
 
   }
 
@@ -37,7 +43,6 @@ class App extends React.Component {
     // console.log(currText);
     // console.log(this.state.text);
     let matchStrsArr = [];
-    // let noMatch = true;
     for (let i = 0; i < moviesArr.length; i++) {
       let currTitle = moviesArr[i].title;
       // console.log(currTitle);
@@ -57,9 +62,48 @@ class App extends React.Component {
     let moviesListStrTemp = matchStrsArr.join(", ")
     console.log(moviesListStrTemp)
     this.setState({ moviesListStr: moviesListStrTemp});
+  }
 
-}
-  
+  handleUserChange(event) {
+    this.setState({ userText: event.target.value });
+  }
+
+  // currUserMovie: null,
+  // userMoviesArr: [],
+  // userText: '',
+  handleUserClick(event) {
+    event.preventDefault();
+    // fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${this.state.text}&key=${YOUTUBE_API_KEY}`)
+    // fetch(`${this.state.text}`)
+    //   .then(res => res.json())
+    //   .then(moviesArr => this.setState({ moviesArr: currMovie, currMovie: currMovie.title[0] }))
+    // console.log(document.body.getElementsByClassName("form-control")[0].value);
+    let newTitle = this.state.userText;
+    // console.log(newTitle);
+    // console.log(this.state.userText);
+    let userMoviesTempArr = [];
+    // for (let i = 0; i < userMoviesTempArr.length; i++) {
+    //   let currTitle = userMoviesTempArr[i].title;
+    //   // console.log(currTitle);
+    //   if (currTitle !== newTitle) {
+    //     // matchStrsArr.push(moviesArr[i].title);
+        userMoviesTempArr.push(newTitle);
+        console.log(userMoviesTempArr);
+        // {this.state.movieList}.push(moviesArr[i].title);
+    //   }
+    // }
+    if (userMoviesTempArr.length === 0) {
+      userMoviesTempArr.push("None");
+    }
+    console.log(userMoviesTempArr);
+    // {this.state.movieList}.push = matchStrArr.slice();
+    this.setState({ userMoviesArr: userMoviesTempArr});
+    // return matchStrsArr;
+    let moviesListStrTemp = userMoviesTempArr.join(", ")
+    console.log(moviesListStrTemp)
+    this.setState({ userMoviesListStr: moviesListStrTemp});
+  }
+
   render() {
     return(
       <div>
@@ -72,18 +116,40 @@ class App extends React.Component {
         </div>
 
         <div className=".search-bar">
+          Search
             <Search
               handleChange={this.handleChange}
               handleClick={this.handleClick}
             />
         </div>
         <div className=".search-results">
-        Search results: 
+          Search results: 
         {/* console.log({this.state.text}) */}
         </div>
         <div>
           {this.state.moviesListStr}
         </div>
+
+        <div>
+          .
+        </div>
+
+        {/* user added */}
+        <div className=".search-bar">
+          Add your favorite movies (comma separated)
+            <AddMovie
+              handleUserChange={this.handleUserChange}
+              handleUserClick={this.handleUserClick}
+            />
+        </div>
+        <div className=".search-results">
+        My added movies results: 
+        {/* console.log({this.state.text}) */}
+        </div>
+        <div>
+          {this.state.userMoviesListStr}
+        </div>
+
         <div>
           .
         </div>
@@ -91,6 +157,7 @@ class App extends React.Component {
       
         <div className=".movie-list">
         <tbody>
+          Current Movie Library
           <tr key="0" className="singleMovieEntry">
             {moviesArr[0].title}
           </tr>
@@ -112,6 +179,20 @@ class App extends React.Component {
   )}
 
 }
+
+var AddMovie = ({ handleUserChange, handleUserClick }) => (
+  <div className="search-bar .form-control">
+    <input
+      className="form-control"
+      type="text"
+      onChange={handleUserChange}
+    />
+    <button className=".search-bar .btn" onClick={handleUserClick}>
+      Add!
+    </button>
+  </div>
+);
+
 
 var Search = ({ handleChange, handleClick }) => (
   <div className="search-bar .form-control">
