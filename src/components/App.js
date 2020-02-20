659602cf0348837fb5e7dc293a3a859c5f3c0c82
nsbernstein52 <57200889+nsbernstein52@users.noreply.github.com>
2020-02-20 12:35:11 -0700

    Debug capturing search result

--- src/components/App.js
+++ src/components/App.js
@@ -5,38 +5,39 @@ class App extends React.Component {
     super(props);
     
     this.state = {
-      movies: [],
-      movie: null,
+      moviesArr: [],
+      currMovie: null,
       text: ''
     };
 
     this.changeCurrMovie = this.changeCurrMovie.bind(this);
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleClick = this.handleClick.bind(this);
 
   }
 
   changeCurrMovie(movieIndex) {
-    this.setState({ movie: movies[movieIndex] })
+    this.setState({ currMovie: moviesArr[movieIndex] })
   }
 
-  handleChange(elem) {
-    this.setState({ text: elem.target.value });
+  handleChange(event) {
+    this.setState({ text: event.target.value });
   }
 
-  handleSubmit() {
+  handleClick(event) {
+    event.preventDefault();
     // fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${this.state.text}&key=${YOUTUBE_API_KEY}`)
-    fetch(`${this.state.text}`)
+    // fetch(`${this.state.text}`)
     //   .then(res => res.json())
-      .then(data => this.setState({ movies: movie, movie: movie.title[0] }))
+    //   .then(moviesArr => this.setState({ moviesArr: currMovie, currMovie: currMovie.title[0] }))
+    console.log(document.body.getElementsByClassName("form-control")[0].value);
+    console.log(this.state.text);
   }
-
-  
   
   render() {
     return(
       <div>
-        <div className="header">
+        <div className=".navbar">
           <h3>
           Movie List
           </h3>
@@ -44,50 +45,53 @@ class App extends React.Component {
         <div className="tbd">
         </div>
 
-        <div className="col-md-6 offset-md-3">
+        <div className=".search-bar">
             <Search
               handleChange={this.handleChange}
-              handleSubmit={this.handleSubmit}
+              handleClick={this.handleClick}
             />
           </div>
           
       
-        <div className="movieList">
+        <div className=".movie-list">
+        <tbody>
           <tr key="0" className="singleMovieEntry">
-            {movies[0].title}
+            {moviesArr[0].title}
           </tr>
           <tr key="1" className="singleMovieEntry">
-            {movies[1].title}
+            {moviesArr[1].title}
           </tr>
           <tr key="2" className="singleMovieEntry">
-            {movies[2].title}
+            {moviesArr[2].title}
           </tr>
           <tr key="3" className="singleMovieEntry">
-            {movies[3].title}
+            {moviesArr[3].title}
           </tr>
           <tr key="4" className="singleMovieEntry">
-            {movies[4].title}
+            {moviesArr[4].title}
           </tr>
+        </tbody>
         </div>
       </div>
   )}
 
 }
 
-var Search = ({ handleChange, handleSubmit }) => (
-  <div className="search-bar form-inline">
+var Search = ({ handleChange, handleClick }) => (
+  <div className="search-bar .form-control">
     <input
       className="form-control"
       type="text"
       onChange={handleChange}
     />
-    <button className="btn hidden-sm-down" onClick={handleSubmit}>
-      <span className="glyphicon glyphicon-search"></span>
+    <button className=".search-bar .btn" onClick={handleClick}>
+      Go!
+      {/* <span className="glyphicon glyphicon-search"></span> */}
     </button>
   </div>
 );
 
-var movies = [
+var moviesArr = [
   {title: 'Mean Girls'},
   {title: 'Hackers'},
   {title: 'The Grey'},
